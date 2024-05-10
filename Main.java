1ff8893be1daa4bdbef0187d7a4a79387f87918a
Claudio Guarrasi <claudio.guarrasi@gmail.com>
2024-05-10 11:30:28 +0200

    feat: add method which initialize a matrix with random one-digit values

--- Main.java
+++ Main.java
@@ -1,5 +1,17 @@
 public class Main{
     public static void main(String args[]){
+        int k, n; //k=colonne, n=righe
+        int a[][] = initMatrix(k, n);
         System.out.println("Hello World");
+    }
+
+    public static int[][] initMatrix(int rows, int columns) {
+        int matrix[][] = new int[rows][columns];
+        for (int i=0; i<rows; i++){
+            for (int j=0; j<columns; j++){
+                matrix[i][j] = (int) (Math.random() * 10);
+            }
+        }
+        return matrix;
     }
 }
