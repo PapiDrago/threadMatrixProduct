f73ec02d9ebd0deab67ea7f8c667eb49f83525bf
Claudio Guarrasi <claudio.guarrasi@gmail.com>
2024-05-10 14:48:49 +0200

    feat: add option to input matrix dimensions from command line

--- Main.java
+++ Main.java
@@ -1,10 +1,13 @@
+import java.util.regex.Pattern;
+
 public class Main{
     public static void main(String args[]){
-        int k, n; //k=colonne, n=righe
-        int a[][] = initMatrix(2, 3);
-        int b[][] = initMatrix(3, 2);
+        int matrices[][][] = initMatricesFromArguments(args);
+        int a[][] = matrices[0];
+        int b[][] = matrices[1];
         printMatrix(a);
         printMatrix(b);
+        //Thread[] threads = new Thread[];
         printMatrix(matrixProd(a, b));
     }
 
@@ -31,7 +34,7 @@ public class Main{
     public static int[][] matrixProd(int[][] a, int[][] b){
         if(a[0].length != b.length) {
             throw new IllegalArgumentException("Il numero di colonne di A "+
-                                    "deve essere uguale al numero di righe di B!");
+                                    "non e' uguale al numero di righe di B!");
         } 
         int c[][] = new int[a.length][b[0].length];
         for(int rowA = 0; rowA<a.length; rowA++){
@@ -44,5 +47,31 @@ public class Main{
             }
         }
         return c;
+    }
+
+    public static int[][][] initMatricesFromArguments(String[] args){
+        
+        if(args.length == 0) {
+            System.out.println("Inizialiazzazione di due matrici 2x2.");
+            int matrices[][][] = new int[2][][];
+            matrices[0] = initMatrix(2, 2);
+            matrices[1] = initMatrix(2, 2);
+            return matrices;
+        }
+        int matrices[][][] = new int[args.length][][];
+        for (int i = 0; i<args.length; i++){
+            System.out.println(args[i]);
+            if(Pattern.matches("[0-9]+,[0-9]+", args[i])){
+                int rows = Integer.parseInt(args[i].split(",")[0]);
+                int cols = Integer.parseInt(args[i].split(",")[1]);
+                int matrix[][] = initMatrix(rows, cols);
+                matrices[i] = matrix; //da gestire '0,0'
+
+            } else {
+                throw new IllegalArgumentException("Gli argomenti facoltativi "
+                +"devono avere la seguente espressione [numero righe],[numero colonne]");
+            }
+        }
+        return matrices;
     }
 }
