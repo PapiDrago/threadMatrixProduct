5ed77feeb2359d628b1d973315dc9e81576ee613
Claudio Guarrasi <claudio.guarrasi@gmail.com>
2024-05-10 11:43:53 +0200

    feat: add method to print a matrix

--- Main.java
+++ Main.java
@@ -1,8 +1,8 @@
 public class Main{
     public static void main(String args[]){
         int k, n; //k=colonne, n=righe
-        int a[][] = initMatrix(k, n);
-        System.out.println("Hello World");
+        int a[][] = initMatrix(2, 2);
+        printMatrix(a);
     }
 
     public static int[][] initMatrix(int rows, int columns) {
@@ -13,5 +13,14 @@ public class Main{
             }
         }
         return matrix;
+    }
+
+    public static void printMatrix(int[][] matrix){
+        for (int i = 0; i<matrix.length; i++){
+            for (int j = 0; j<matrix[0].length; j++){
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
     }
 }
