3a405eafd0a9263239b1d46efe17b54e6ffb6a51
Claudio Guarrasi <claudio.guarrasi@gmail.com>
2024-05-10 13:01:40 +0200

    feat: add 2x2 matrix product

--- Main.java
+++ Main.java
@@ -2,7 +2,10 @@ public class Main{
     public static void main(String args[]){
         int k, n; //k=colonne, n=righe
         int a[][] = initMatrix(2, 2);
+        int b[][] = initMatrix(2, 2);
         printMatrix(a);
+        printMatrix(b);
+        printMatrix(matrixProd(a, b));
     }
 
     public static int[][] initMatrix(int rows, int columns) {
@@ -16,11 +19,24 @@ public class Main{
     }
 
     public static void printMatrix(int[][] matrix){
+        System.out.print("\n");
         for (int i = 0; i<matrix.length; i++){
-            for (int j = 0; j<matrix[0].length; j++){
+            for (int j = 0; j<matrix[0].length; j++){ //itero sulla riga i-esima
                 System.out.print(matrix[i][j] + " ");
             }
             System.out.println();
         }
+    }
+
+    public static int[][] matrixProd(int[][] a, int[][] b){
+        int c[][] = new int[a.length][b[0].length];
+        for(int i = 0; i<a.length; i++){
+            for(int j = 0; j<b[0].length; j++){
+                for (int k = 0; k < a[0].length; k++){
+                    c[i][j] = c[i][j] + a[i][k] * b[k][j];
+                }
+        }
+        }
+        return c;
     }
 }
